c782eb2b16c455cfdc77b40337a8ce3e58da362a
leesure <arm125@qq.com>
2023-02-28 18:05:30 +1100

    vscode workspace

--- src/router/index.js
+++ src/router/index.js
@@ -78,11 +78,11 @@ const router = new VueRouter({
 });
 
 
-//全局导航守卫
-router.beforeEach((to, from, next) => {
-  document.title = to.matched[0].meta.title;
-  next();
-});
+// //全局导航守卫
+// router.beforeEach((to, from, next) => {
+//   document.title = to.matched[0].meta.title;
+//   next();
+// });
 
 // 3.将router对象传入到Vue实例
 export default router
